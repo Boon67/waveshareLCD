e94c08d98b6c178be34ed7324bd68d152df7a80f
Terrance Boon <boon67@msn.com>
2023-08-27 13:29:50 -0500

    Cleanup

--- main.py
+++ main.py
@@ -1,43 +1,82 @@
 import LCD_1in44
 import LCD_Config
+from threading import Thread
+import socket, time
+from datetime import datetime
+
+hostname=socket.gethostname()
+IPAddr=socket.gethostbyname(hostname)
 
 from PIL import Image,ImageDraw,ImageFont,ImageColor
-
-#try:
-def main():
+def getLCD():
     LCD = LCD_1in44.LCD()
-
     print ("**********Init LCD**********")
     Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
     LCD.LCD_Init(Lcd_ScanDir)
-    LCD.LCD_Clear()
+    return LCD
 
-    image = Image.new("RGB", (LCD.width, LCD.height), "WHITE")
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+    
+def clock(draw):
+    while True:
+        LCD=getLCD()
+        now= datetime.now()
+        current_time=now.strftime("%H:%M:%S")
+        draw.text((10, 10), current_time, fill = "WHITE")
+        print(current_time)
+        LCD.LCD_ShowImage(image,0,0)
+        time.sleep(1)
+        
+def main():
+#try:
+    LCD=getLCD()
+    LCD.LCD_Clear()
+    #image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+    image = Image.open('cblogo.png')
+    image = image.resize((128,128))
     draw = ImageDraw.Draw(image)
     #font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 16)
-    print ("***draw line")
-    draw.line([(0,0),(127,0)], fill = "BLUE",width = 5)
-    draw.line([(127,0),(127,127)], fill = "BLUE",width = 5)
-    draw.line([(127,127),(0,127)], fill = "BLUE",width = 5)
-    draw.line([(0,127),(0,0)], fill = "BLUE",width = 5)
+    print ("***draw outline")
+    draw.line([(0,0),(127,0)], fill = "WHITE",width = 1)
+    draw.line([(127,0),(127,127)], fill = "WHITE",width = 1)
+    draw.line([(127,127),(0,127)], fill = "WHITE",width = 1)
+    draw.line([(0,127),(0,0)], fill = "WHITE",width = 1)
     print ("***draw rectangle")
     draw.rectangle([(18,10),(110,20)],fill = "RED")
 
     print ("***draw text")
-    #draw.text((33, 22), 'WaveShare ', fill = "RED")
-    #draw.text((32, 36), 'Electronic ', fill = "BLUE")
-    #draw.text((28, 48), '1.44inch LCD ', fill = "BLUE")
-
-    LCD.LCD_ShowImage(image,0,0)
-    LCD_Config.Driver_Delay_ms(500)
+    #LCD.LCD_ShowImage(image,0,0)
+    while True:
+        #image = Image.open('time.bmp')
+        LCD.LCD_ShowImage(image,0,0)
+        now = datetime.now()
+        current_time=now.strftime("%H:%M:%S")
+        draw.rectangle([(10,10),(110,20)],fill = "BLACK")
+        draw.text((10, 10), current_time, fill = "WHITE")
+        draw.text((10, 100), 'Host:' + hostname, fill = "GREEN")
+        draw.text((10, 110), 'IP: ' + get_ip(), fill = "BLUE")
+        LCD.LCD_ShowImage(image,0,0)
+        LCD_Config.Driver_Delay_ms(0)
+        time.sleep(1)
 
     #image = Image.open('time.bmp')
     #LCD.LCD_ShowImage(image,0,0)
-	
-	#while (True):
+    #while (True):	
 	
 if __name__ == '__main__':
     main()
+    #Thread(target=clock).start()
 
 #except:
 #	print("except")

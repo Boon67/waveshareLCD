06bb0f854ab2b7e5f30ed8cd8ba04725ce64e6d8
Terrance Boon <boon67@msn.com>
2023-09-27 22:34:09 +0100

    Added install script

---
+++ install_drivers.sh
@@ -0,0 +1,21 @@
+#Open the Raspberry Pi terminal and run the following command
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz
+tar zxvf bcm2835-1.71.tar.gz
+cd bcm2835-1.71/
+sudo ./configure && sudo make && sudo make check && sudo make install
+# For more information, please refer to the official website: http://www.airspayce.com/mikem/bcm2835/
+
+git clone https://github.com/WiringPi/WiringPi
+cd WiringPi
+./build
+gpio -v
+
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo apt-get install python3-pil
+sudo apt-get install python3-numpy
+sudo pip3 install RPi.GPIO
+sudo pip3 install spidev
+
+
+
